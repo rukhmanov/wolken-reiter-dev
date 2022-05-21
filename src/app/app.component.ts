ba5aad9b134abdd71e262f84f2ad57e39cf3021d
ruhmanov_as <@ruhmanov_as>
2022-05-22 01:59:30 +0300

    extra fixes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,27 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { EntryCollection } from 'contentful';
+import { IMainPageFields } from 'src/app/contentful-type';
 import { environment } from 'src/environments/environment';
+import { ContentfulService } from './contentful.service';
+import {MatToolbarHarness} from '@angular/material/toolbar/testing';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   url = environment.api
   title = 'wolken-reiter';
-  // euphonious-zabaione-4d975e
+  content!: EntryCollection<unknown>
+
+  constructor(private contentful: ContentfulService) {}
+
+  ngOnInit(): void {
+    this.contentful.getContent
+    .then((content: EntryCollection<unknown>) => {
+      this.content = content
+      console.log(content)
+    })
+  }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,14 @@
 
 export const environment = {
   production: false,
-  api: 'http//:dev.server.io'
+  api: 'http//:dev.server.io',
+  contentful: {
+    spaceId: '5n5pcoqyb97y',
+    accessTokenDelivery: 'A7ohRahkpDP-tjxzqWKG8cACFyZV8oFr_eTLDEwg2jk',
+    accessTokenPreview: 'aa0TGv9o8WZhRcawMXo41boeuRd1l6ob9Ko9WvcCFn4',
+    firstAccessToken: 'CFPAT-MPJiY70A5ZyR6PzJe0n5x3TssOUYVLXXLmJ6AMIpQ_I',
+    contentfulEnviroment: 'master'
+  }
 };
 
 /*
