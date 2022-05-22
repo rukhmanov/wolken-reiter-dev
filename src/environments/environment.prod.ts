f1d2cc67967ebaf73ce42c96c37bbd14fec959af
ruhmanov_as <@ruhmanov_as>
2022-05-22 13:02:54 +0300

    mat-panel is added

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,11 @@
 export const environment = {
   production: true,
-  api: 'https//:server.io'
+  api: 'https//:server.io',
+  contentful: {
+    spaceId: '5n5pcoqyb97y',
+    accessTokenDelivery: 'A7ohRahkpDP-tjxzqWKG8cACFyZV8oFr_eTLDEwg2jk',
+    accessTokenPreview: 'aa0TGv9o8WZhRcawMXo41boeuRd1l6ob9Ko9WvcCFn4',
+    firstAccessToken: 'CFPAT-MPJiY70A5ZyR6PzJe0n5x3TssOUYVLXXLmJ6AMIpQ_I',
+    contentfulEnviroment: 'master'
+  }
 };
