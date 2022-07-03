31567a2d9937de11b8d84236404fcacd9f3483a1
ruhmanov_as <@ruhmanov_as>
2022-07-03 14:01:30 +0300

    fix

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  baseUrl: 'https://wr-backand.herokuapp.com',
+  baseUrl: 'https://wrback.herokuapp.com',
   contentful: {
     spaceId: '5n5pcoqyb97y',
     accessTokenDelivery: 'A7ohRahkpDP-tjxzqWKG8cACFyZV8oFr_eTLDEwg2jk',
